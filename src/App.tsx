cbf887e3765d5f24de7d5c33910e3ced8c73531d
noda-chpartners <noda@ch-partners.co.jp>
2026-06-03 19:05:44 +0900

    add texture

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,22 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 
 
+import textureOverlay from '@/components/assets/テクスチャーざらつき.png';
+
 function App() {
   return (
     <I18nextProvider i18n={i18n}>
       <BrowserRouter basename={__BASE_PATH__}>
+        {/* テクスチャオーバーレイ */}
+        <div
+          aria-hidden
+         className="pointer-events-none fixed inset-0 z-[60] opacity-30 mix-blend-multiply"
+          style={{
+            backgroundImage: `url(${textureOverlay})`,
+            backgroundRepeat: 'repeat',
+            backgroundSize: 'min(80vw, 900px)',
+          }}
+        />
         <AppRoutes />
       </BrowserRouter>
     </I18nextProvider>
