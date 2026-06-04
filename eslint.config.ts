28bca0e07c852b208129a94a2f7e85750a45010e
noda-chpartners <noda@ch-partners.co.jp>
2026-06-04 15:30:48 +0900

    aaaa

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,7 +5,6 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import routeElementPlugin from './eslint-rules/route-element-jsx.js'
 
-
 const autoImportGlobals = {
   // React
   React: 'readonly',
