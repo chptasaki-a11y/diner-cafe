ae25c3b11d19732ac42969f5b0fcc7cb4cd3d41f
noda-chpartners <noda@ch-partners.co.jp>
2026-06-04 14:59:17 +0900

    fix

--- eslint.config.ts
+++ eslint.config.ts
@@ -7,6 +7,7 @@ import routeElementPlugin from './eslint-rules/route-element-jsx.js'
 
 const autoImportGlobals = {
   // React
+  
   React: 'readonly',
   useState: 'readonly',
   useEffect: 'readonly',
